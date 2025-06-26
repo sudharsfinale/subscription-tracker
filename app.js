1a02898c9c7cc0de4d3d68e7cdf049902c7c5e85
sudharsfinale <sudharsfinale@gmail.com>
2025-06-26 19:02:30 +0530

    Setup arcjet to detect too many requests and bot detection

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/db.js";
 import errorMiddleWare from "./middlewares/errrors.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // parses cookies attached to the request and populates `req.cookies`
 app.use(cookieParser()); 
+
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
